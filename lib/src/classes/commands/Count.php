25c2cad871e98982116269c61df0cfae6f7817fd
Nicolas Choquet <nicolas.choquet.ext@orange.com>
2020-02-27 11:51:50 +0100

    add method into count command

--- lib/src/classes/commands/Count.php
+++ lib/src/classes/commands/Count.php
@@ -14,12 +14,18 @@ class Count {
 
     private ?Platform $platformHelper = null;
 
-    const LINES = 1;
     const CLASSES = 0;
+    const LINES = 1;
+    const TEMPLATES = 2;
 
     protected array $directories = [
-        __DIR__.'/../../../src',
-        __DIR__.'/../../../scripts',
+        'src' => __DIR__.'/../../../src',
+        'scripts' => __DIR__.'/../../../scripts',
+        'classes' => __DIR__.'/../../../src/classes',
+        'main' => __DIR__.'/../../../src/main',
+        'interfaces' => __DIR__.'/../../../src/interfaces',
+        'traits' => __DIR__.'/../../../src/traits',
+        'templates' => __DIR__.'/../../../templates',
     ];
 
     protected function read_dir(string $directory, int &$n, int $type): void {
@@ -29,11 +35,18 @@ class Count {
                 if (is_file($directory . '/' . $elem))
                     switch($type) {
                         case self::CLASSES:
-                            $n++;
+                            if(substr($elem, strlen($elem) - strlen('.php'), strlen('.php')) === '.php') {
+                                $n++;
+                            }
                             break;
                         case self::LINES:
                             $content = file_get_contents($directory . '/' . $elem);
                             $n += count(explode("\n", $content));
+                            break;
+                        case self::TEMPLATES:
+                            if(substr($elem, strlen($elem) - strlen('.html'), strlen('.html')) === '.html') {
+                                $n++;
+                            }
                             break;
                         default:
                             break;
@@ -42,18 +55,53 @@ class Count {
             endif;
     }
 
-    public function number_of_classes() {
+    public function sources() {
         $n = 0;
-        foreach ($this->directories as $directory) $this->read_dir($directory, $n, self::CLASSES);
+        $dirs = [
+            $this->directories['src'] => self::CLASSES,
+            $this->directories['scripts'] => self::CLASSES,
+            $this->directories['templates'] => self::TEMPLATES,
+        ];
+
+        foreach ($dirs as $dir => $mode) $this->read_dir($dir, $n, $mode);
+
+        echo $n." sources".($n > 1 ? 'es' : '')." found !\n";
+    }
+
+    public function classes() {
+        $n = 0;
+        $this->read_dir($this->directories['classes'], $n, self::CLASSES);
+        $this->read_dir($this->directories['main'], $n, self::CLASSES);
 
         echo $n." class".($n > 1 ? 'es' : '')." found !\n";
     }
 
-    public function number_of_lines() {
+    public function lines() {
         $n = 0;
-        foreach ($this->directories as $directory) $this->read_dir($directory, $n, self::LINES);
+        $this->read_dir($this->directories['src'], $n, self::LINES);
 
         echo $n." line".($n > 1 ? 'es' : '')." found !\n";
+    }
+
+    public function scripts() {
+        $n = 0;
+        $this->read_dir($this->directories['scripts'], $n, self::CLASSES);
+
+        echo $n." script".($n > 1 ? 'es' : '')." found !\n";
+    }
+
+    public function traits() {
+        $n = 0;
+        $this->read_dir($this->directories['traits'], $n, self::CLASSES);
+
+        echo $n." trait".($n > 1 ? 'es' : '')." found !\n";
+    }
+
+    public function templates() {
+        $n = 0;
+        $this->read_dir($this->directories['templates'], $n, self::TEMPLATES);
+
+        echo $n." template".($n > 1 ? 'es' : '')." found !\n";
     }
 
     public function help() {

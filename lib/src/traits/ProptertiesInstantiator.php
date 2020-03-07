dab62f7512d012785a01fe12fc2874bd2131e561
Nicolas Choquet <nicolachoquet06250@gmail.com>
2020-03-07 16:03:06 +0100

    gestion des propriétés statics pour l'injection de dépendences

--- lib/src/traits/ProptertiesInstantiator.php
+++ lib/src/traits/ProptertiesInstantiator.php
@@ -9,19 +9,33 @@ use ReflectionException;
 use ReflectionProperty;
 
 trait ProptertiesInstantiator {
-    /**
-     * @param ReflectionProperty $property
-     * @throws ReflectionException
-     */
-    private final function instantiate_properties(ReflectionProperty $property): void {
-        if (!is_null($property->getType()) && !in_array($property->getType()->getName(), static::$primary_type_array)) {
-            $_class = $property->getType()->getName();
-            $propertyClass = new ReflectionClass($_class);
-            $className = $propertyClass->getName();
+	/**
+	 * @param ReflectionProperty $property
+	 * @throws ReflectionException
+	 */
+	private final function instantiate_properties(ReflectionProperty $property): void {
+		if (!is_null($property->getType()) && !in_array($property->getType()->getName(), static::$primary_type_array)) {
+			$_class = $property->getType()->getName();
+			$propertyClass = new ReflectionClass($_class);
+			$className = $propertyClass->getName();
 
-            if ($propertyClass->hasMethod('init')) $this->{$property->getName()} = $className::init();
-            elseif ($propertyClass->hasMethod('create')) $this->{$property->getName()} = $className::create();
-            else $this->{$property->getName()} = new $className();
-        }
-    }
+			if($property->isStatic()) {
+				if ( $propertyClass->hasMethod( 'init' ) ) {
+					static::${$property->getName()} = $className::init();
+				} elseif ( $propertyClass->hasMethod( 'create' ) ) {
+					static::${$property->getName()} = $className::create();
+				} else {
+					static::${$property->getName()} = new $className();
+				}
+			} else {
+				if ( $propertyClass->hasMethod( 'init' ) ) {
+					$this->{$property->getName()} = $className::init();
+				} elseif ( $propertyClass->hasMethod( 'create' ) ) {
+					$this->{$property->getName()} = $className::create();
+				} else {
+					$this->{$property->getName()} = new $className();
+				}
+			}
+		}
+	}
 }
